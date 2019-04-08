5cdabe5709b18024a1254b303dee38c5da4d8cd7
qzed <qzed@users.noreply.github.com>
2019-04-08 12:58:02 +0200

    Fix dependencies

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 
 setup(
     name='surface_dtx',
-    version='0.1.4',
+    version='0.1.5',
     packages=['surface_dtx'],
     package_dir={'surface_dtx': 'dtx'},
-    requires=['pydbus', 'toml'],
+    requires=['dbus', 'toml'],
 )
